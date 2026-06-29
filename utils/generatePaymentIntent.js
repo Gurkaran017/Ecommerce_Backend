60cd3eb3edd5d8711b4e885973b32be015f9db07
gurkaran.singh <gurkaran.singh@mastersunion.org>
2026-06-29 15:31:59 +0530

    stripe payment error fix

--- utils/generatePaymentIntent.js
+++ utils/generatePaymentIntent.js
@@ -1,9 +1,12 @@
 import database from "../database/db.js";
 import Stripe from "stripe";
 
-const stripe = Stripe("PASTE_YOUR_STRIPE_SECRET_KEY");
-
 export async function generatePaymentIntent(orderId, totalPrice) {
+  // Initialize Stripe here
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  console.log("Stripe Key:", process.env.STRIPE_SECRET_KEY);
+
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: totalPrice * 100,
@@ -11,13 +14,22 @@ export async function generatePaymentIntent(orderId, totalPrice) {
     });
 
     await database.query(
-      "INSERT INTO payments (order_id, payment_type, payment_status, payment_intent_id) VALUES ($1, $2, $3, $4) RETURNING *",
+      `INSERT INTO payments
+      (order_id, payment_type, payment_status, payment_intent_id)
+      VALUES ($1, $2, $3, $4) RETURNING *`,
       [orderId, "Online", "Pending", paymentIntent.client_secret]
     );
 
-    return { success: true, clientSecret: paymentIntent.client_secret };
+    return {
+      success: true,
+      clientSecret: paymentIntent.client_secret,
+    };
   } catch (error) {
     console.error("Payment Error:", error.message || error);
-    return { success: false, message: "Payment Failed." };
+
+    return {
+      success: false,
+      message: "Payment Failed.",
+    };
   }
 }
